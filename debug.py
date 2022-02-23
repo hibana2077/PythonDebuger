60c4615411c682f3fed12333f3cc4bf596fe7d44
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2022-02-23 10:21:37 +0800

    Add files via upload

---
+++ debug.py
@@ -0,0 +1,30 @@
+import collections
+class debuger:
+    def __init__(self) -> None:
+        self.watch=list()
+    def showver(self):
+        print("-----------------")
+        print("|  version 0.1  |")
+        print("-----------------")
+    def vla(self,*new):
+        for t in list(new):
+            self.watch.append(t)
+    def svl(self):
+        for t in self.watch:
+            print(t,type(t))
+    def vlsame(self):
+        test=[(name,times) for name,times in list(collections.Counter(self.watch).most_common(len(self.watch))) if times >=2]
+        print("-----------------")
+        print("|  most common  |")
+        print("-----------------")
+        for t in test:
+            print(f"variable value: {t[0]}  variable times: {t[1]}")
+    def egg(self):
+        ar=[" _______________________ ","< Hello, bovine world!  >"," -----------------------",
+        "        \   ^__^","         \  (oo)\_______","            (__)\       )\/\ ",
+        "                ||----w |","                ||     ||"]
+        for t in ar:print(t)
+    def linux(self):
+        linux=["< Hello, linux world!  >"," -----------------------","   \ ","    \ ",
+        "        .--.","       |o_o |","       |:_/ |","      //   \ \ ","     (|     | )","    /'\_   _/`\ ","    \___)=(___/"]
+        for t in linux:print(t)
